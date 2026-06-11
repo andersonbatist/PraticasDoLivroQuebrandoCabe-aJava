b52cc5dd17584f0264e2ca61559ccc048a026974
Anderson Batista <andersonbatista.home@gmail.com>
2026-06-11 18:37:23 -0300

    Novas práticas adicionadas e novos conhecimentos aprendidos

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,6 +1,33 @@
 public class MyFirstApp {
     public static void main (String [] args){
+
+        //Escrevendo uma mensagem na tela
+
         System.out.println("I rule!");
         System.out.println("The world");
+
+        //Adicionando um número aleatório a uma variável (Pense em "math" como se fosse "Casar")
+
+        double numero = Math.random();
+        System.out.println(numero);
+
+        // Loop
+
+        int x = 0;
+        while (x < 100) {
+            x = x + 1;
+            System.out.println(x);
+        }
+
+        //Branching
+
+        int preco = 1001;
+
+        if (preco <= 1000) {
+            System.out.println("Vou comprar");
+        } else {
+            System.err.println("Não vou comprar");
+        }
+
     }
 }
