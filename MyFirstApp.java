ce593dc0112886e83bef90a3c803fae522edc93d
Anderson Batista <andersonbatista.home@gmail.com>
2026-06-15 06:37:43 -0300

    Treinando Loops

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,33 +1,49 @@
 public class MyFirstApp {
     public static void main (String [] args){
+        /*
+                //Escrevendo uma mensagem na tela
 
-        //Escrevendo uma mensagem na tela
+                System.out.println("I rule!");
+                System.out.println("The world");
 
-        System.out.println("I rule!");
-        System.out.println("The world");
+                //Adicionando um número aleatório a uma variável (Pense em "math" como se fosse "Casar")
 
-        //Adicionando um número aleatório a uma variável (Pense em "math" como se fosse "Casar")
+                double numero = Math.random();
+                System.out.println(numero);
 
-        double numero = Math.random();
-        System.out.println(numero);
+                // Loop
 
-        // Loop
+                int x = 0;
+                while (x < 100) {
+                    x = x + 1;
+                    System.out.println(x);
+                }
 
-        int x = 0;
-        while (x < 100) {
-            x = x + 1;
-            System.out.println(x);
-        }
+                //Branching
 
-        //Branching
+                int preco = 1001;
 
-        int preco = 1001;
+                if (preco <= 1000) {
+                    System.out.println("Vou comprar");
+                } else {
+                    System.err.println("Não vou comprar");
+                }
 
-        if (preco <= 1000) {
-            System.out.println("Vou comprar");
+        */
+       int contador = 101;
+        System.out.println("INICIANDO O CONTADOR");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        
+        if (contador <= 99) {
+            System.out.println("Seu número é " + contador + ", ou seja, ele é menor que 100\nEntão, vamos contar:");
+            while (contador <= 99) {
+                System.out.println(contador);
+                contador += 1;
+            }
         } else {
-            System.err.println("Não vou comprar");
+            System.err.println("Seu número é maior que 100");
         }
-
     }
 }
---
+++ lacosRepeticao.java
@@ -0,0 +1,10 @@
+public class lacosRepeticao {
+    int contador = 20;
+
+    public void Contar() {
+
+        while (contador > 18) {
+            System.err.println(contador);
+        }
+    }
+}
