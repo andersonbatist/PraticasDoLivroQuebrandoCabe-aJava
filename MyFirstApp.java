b33fbfee75261ad7d45fc1f158bf309d9be4c418
Anderson Batista <andersonbatista.home@gmail.com>
2026-06-15 11:50:14 -0300

    Canção infantil 10 Green Bottles

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -30,20 +30,45 @@ public class MyFirstApp {
                 }
 
         */
-       int contador = 101;
-        System.out.println("INICIANDO O CONTADOR");
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
-        
-        if (contador <= 99) {
-            System.out.println("Seu número é " + contador + ", ou seja, ele é menor que 100\nEntão, vamos contar:");
-            while (contador <= 99) {
-                System.out.println(contador);
-                contador += 1;
+
+        /*
+            int contador = 101;
+                System.out.println("INICIANDO O CONTADOR");
+                System.out.println(" ");
+                System.out.println(" ");
+                System.out.println(" ");
+                
+                if (contador <= 99) {
+                    System.out.println("Seu número é " + contador + ", ou seja, ele é menor que 100\nEntão, vamos contar:");
+                    while (contador <= 99) {
+                        System.out.println(contador);
+                        contador += 1;
+                    }
+                } else {
+                    System.err.println("Seu número é maior que 100");
+                }
+        */
+
+        int bottlesNum = 10;
+        String word = "bottles";
+
+        while (bottlesNum > 0) {
+
+            if (bottlesNum == 1) {
+                word = "bottle";
             }
-        } else {
-            System.err.println("Seu número é maior que 100");
+
+                System.out.println(bottlesNum + " green " + word + " hanging on the wall,");
+                System.out.println(bottlesNum + " green " + word + " hanging on the wall,");
+                System.out.println("And if one green bottle should accidentally fall,");
+                bottlesNum = bottlesNum - 1;
+
+            if (bottlesNum > 0) {
+
+                System.out.println("There'll be " + bottlesNum + " green " + word + " hanging on the wall.\n ");
+            } else {
+                System.out.println("There'll be no green bottles hanging on the wall.");
+            }
         }
     }
 }
