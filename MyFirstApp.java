a354945ad996ffaef915604d4e8badaad2c66534
andersonbatist <andersonbatista.home@gmail.com>
2026-08-17 19:57:39 -0300

    Dando continuidade nas lições

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -345,7 +345,25 @@ public class MyFirstApp {
         
         //Quebra-cabeça na Piscina
 
-         
+
+        Echo e1 = new Echo();
+        Echo e2 = new Echo();
+
+        int x = 0;
+
+        while (x < 5) {
+            e1.hello();
+            e1.count = e1.count + 1;
+            if (x > 4) {
+                e2.count =  e2.count + 1;
+            }
+            if (x > 1) {
+                e2.count = e2.count + e1.count;
+            }
+            x = x + 1;
+        }
+        System.out.println(e2.count);
+
 
         
 
